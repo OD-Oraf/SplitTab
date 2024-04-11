460c459adc273f232320ffa7bd2769949e5981ef
od oraf <poseidonog@gmail.com>
2024-04-11 13:57:28 -0400

    Initial Commit

---
+++ SplitTab/SplitTabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitTabApp.swift
+//  SplitTab
+//
+//  Created by OD Orafidiya on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SplitTabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
